b9a30e0cf5a6b8e98e0d3ea080e637ee9aa5e3c4
soham-haldankar <soham.s.haldankar@gmail.com>
2026-07-27 05:13:12 -0700

    tables schema

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-27T03:04:25-0700",
+    date = "2026-07-27T03:42:08-0700",
     comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
