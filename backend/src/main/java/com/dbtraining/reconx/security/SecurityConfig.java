6460a7aad223257221327d244224ad4b1967476b
Soham Haldankar <soham@Sohams-MacBook-Air.local>
2026-07-31 21:38:11 +0530

    securityconfig changes reverted back

--- backend/src/main/java/com/dbtraining/reconx/security/SecurityConfig.java
+++ backend/src/main/java/com/dbtraining/reconx/security/SecurityConfig.java
@@ -71,15 +71,15 @@ public class SecurityConfig {
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
-                            "/api/v1/auth/login",
-                            "/actuator/health/**",
-                            "/actuator/info",
-                            "/actuator/prometheus",
-                            "/swagger-ui.html",
-                            "/swagger-ui/**",
-                            "/v3/api-docs/**",
-                            "/h2/**"
-                    ).permitAll()
+                        "/auth/login",
+                        "/actuator/health/**",
+                        "/actuator/info",
+                        "/actuator/prometheus",
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/h2/**"
+                ).permitAll()
                 .requestMatchers(HttpMethod.GET,    "/v1/trades/**").hasAnyRole("VIEWER","TRADER","RECON_ANALYST","ADMIN")
                 .requestMatchers(HttpMethod.POST,   "/v1/trades").hasAnyRole("TRADER","ADMIN")
                 .requestMatchers(HttpMethod.PUT,    "/v1/trades/**").hasAnyRole("TRADER","ADMIN")
